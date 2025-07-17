cfe66cb2621f71147eaf3b56522624e91142b826
EunJiJung <bianbbc87@gmail.com>
2025-07-17 13:51:49 +0900

    feat: add room get messages logic with DynamoDB and error handling (#8)
    Signed-off-by: EunJiJung <bianbbc87@gmail.com>

---
+++ backend/http/room/message/get/handler.ts
@@ -0,0 +1,47 @@
+// lambda function to get messages in a room
+import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from "aws-lambda";
+import { getMessages } from "@/http/room/message/get/service";
+import { AppError } from "@/common/errors/AppError";
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    const roomId = event.pathParameters?.roomId;
+
+    if (!roomId) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({
+                message: "Room ID is required"
+            })
+        };
+    }
+
+    try {
+        const response = await getMessages(roomId);
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify(response)
+        };
+
+    } catch (error) {
+        if (error instanceof AppError) {
+            // console.error("AppError:", error);
+            
+            return {
+                statusCode: error.statusCode,
+                body: JSON.stringify({
+                    message: error.message
+                })
+            };
+        }
+
+        // console.error("Unexpected error:", error);
+
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                message: "Internal server error"
+            })
+        };
+    }
+};
