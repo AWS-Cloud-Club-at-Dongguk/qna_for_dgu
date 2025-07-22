8d07200e5d4ff623de661b8c07b354e5891d5317
EunJiJung <bianbbc87@gmail.com>
2025-07-22 18:19:52 +0900

    setting: set layout outlet of react router dom (#12)
    Signed-off-by: EunJiJung <bianbbc87@gmail.com>

--- frontend/src/config/routes.tsx
+++ frontend/src/config/routes.tsx
@@ -1,8 +1,15 @@
-import RoomDetailPage from "@/features/room/pages/RoomDetailPage"
+import Layout from "@/shared/components/layouts/Layout";
+import RoomChatPage from "@/features/room/pages/RoomChatPage";
 
 export const routes = [
-    {
-        path: "/",
-        element: <RoomDetailPage />
-    },
+  {
+    path: "/",
+    element: <Layout />, // 이 안에 Outlet이 있어야 함
+    children: [
+      {
+        path: "",
+        element: <RoomChatPage />,
+      },
+    ],
+  },
 ];
