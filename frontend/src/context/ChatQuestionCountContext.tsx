2ebe0389a062abfc641228ac25fc3877e47ac723
EunJiJung <bianbbc87@gmail.com>
2025-07-24 02:21:15 +0900

    feat: add chat questions count context (#13)
    Signed-off-by: EunJiJung <bianbbc87@gmail.com>

---
+++ frontend/src/context/ChatQuestionCountContext.tsx
@@ -0,0 +1,26 @@
+import { createContext, useContext, useState } from "react";
+
+type ChatQuestionCountContextType = {
+    questionCount: number
+    setQuestionCount: (count: number) => void
+}
+
+export const ChatQuestionCountContext = createContext<ChatQuestionCountContextType | undefined>(undefined);
+
+export const ChatQuestionCountProvider = ({ children }: { children: React.ReactNode }) => {
+    const [questionCount, setQuestionCount] = useState<number>(0);
+
+    return (
+        <ChatQuestionCountContext.Provider value={{ questionCount, setQuestionCount }}>
+            {children}
+        </ChatQuestionCountContext.Provider>
+    );
+};
+
+export const useChatQuestionCount = () => {
+    const context = useContext(ChatQuestionCountContext);
+    if (!context) {
+        throw new Error('useChatQuestionCount must be used within a ChatQuestionCountProvider');
+    }
+    return context;
+}
