cebe720e836e9dce4b78edbff8768e1fb596ec47
EunJiJung <bianbbc87@gmail.com>
2025-07-24 02:22:55 +0900

    feat: definition api, client type and dto adaptor (#13)
    Signed-off-by: EunJiJung <bianbbc87@gmail.com>

---
+++ frontend/src/features/room/adapters/dtoAdaptor.ts
@@ -0,0 +1,34 @@
+import {
+    ChatMessageData,
+    ChatMessageDto,
+    ChatMessageListData,
+    ChatRoomInfoData,
+    ChatRoomInfoDto,
+} from '@/features/room/types'
+
+// const liked = getLikedMessageIds() // localStorage에서 가져오기
+export const toChatMessageData = (dto: ChatMessageDto): ChatMessageData => {
+    return {
+        ...dto,
+        isLiked: false, // 초기값은 false로 설정
+    }
+}
+
+// createdAt 기준으로 정렬된 메시지 리스트를 반환
+export const toChatMessageListData = (
+    messages: ChatMessageDto[]
+): ChatMessageListData => {
+    return messages
+        .sort(
+            (a, b) =>
+                new Date(a.createdAt).getTime() -
+                new Date(b.createdAt).getTime()
+        )
+        .map(toChatMessageData)
+}
+
+export const toChatRoomInfoData = (dto: ChatRoomInfoDto): ChatRoomInfoData => {
+    return {
+        ...dto,
+    }
+}
---
+++ frontend/src/features/room/types/index.ts
@@ -0,0 +1,39 @@
+/** for api */
+export interface ChatRoomInfoDto {
+    id: string
+    title: string
+    description?: string
+    bannerUrl?: string
+    iconUrl?: string
+}
+
+export interface ChatMessageDto {
+    id: string
+    nickname: string
+    content: string
+    createdAt: string
+    likes: number
+}
+
+export interface RequestChatMessageDto {
+    clientId: string
+    nickname: string
+    content: string
+}
+
+export type ChatMessageListDto = {
+    messages: ChatMessageDto[]
+}
+
+/** use only client side */
+export type ChatMessageData = ChatMessageDto & {
+    isLiked: boolean
+}
+
+export type ChatRoomInfoData = ChatRoomInfoDto;
+
+export type ChatMessageListData = ChatMessageData[]
+
+export type ChatRoomData = ChatRoomInfoData & {
+    messages: ChatMessageListData
+}
